1c14564e5c0b39c4e20aa27ecb8eb2db902cc98c
Levi Daymon <levithehandyman@outlook.com>
2021-03-15 15:49:29 -0400

    added a bunch of static changes, test suite

--- src/App.js
+++ src/App.js
@@ -1,15 +1,68 @@
-
+import React, {Component} from 'react';
 import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-function App() {
-  return (
-    <div>
+var FontAwesome = require('react-fontawesome');
+
+// components:
+
+class Header extends Component {
+  render() {
+    return(
       <div id='header'>
-        <h1 class='text'>番茄时钟</h1>
-        <h5 class='text'>tomato clock</h5>
+        <h1 className='text'>番茄时钟</h1>
+        <h3 className='text'>Fānqié Shízhōng</h3>
       </div>
-    </div>
-  );
+    );
+  }
+}
+
+class Timer extends Component {
+  render() {
+    return(
+      <div className='clock-container'>
+        <button className='increment'>
+          <FontAwesome className="far fa-caret-square-up"/>
+        </button>
+          <span className='timer-display'>25:00</span>
+        <button className='decriment'>
+          <FontAwesome className="far fa-caret-square-down" />
+        </button>
+      </div>
+    );
+  }
+}
+
+class Display extends Component{
+  render() {
+    return(
+      <div className='display'>
+        <h2>time left</h2>
+        <br />
+        <span>0:00</span>
+        <br />
+        <button className='action-button'>
+          <FontAwesome className="fas fa-stopwatch" />
+        </button>
+        <button className='action-button'>
+          <FontAwesome className="fas fa-sync-alt" />
+        </button>
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Timer id=''>break</Timer>
+        <Timer >session</Timer>
+        <Display />
+      </div>
+    );
+  }
 }
 
 export default App;
